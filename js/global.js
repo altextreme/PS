3aa547a6f1b92077eb194bde0c38d5ce79ce29cb
Anthony <atackett@atackett.local>
2015-08-26 12:29:08 -0500

    Added global.js

---
+++ js/global.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    
+    $.ajaxSetup({
+        scriptCharset: "utf-8",
+        contentType: "application/json; charset=utf-8"
+    });
+
+    $.getJSON('http://whateverorigin.org/get?url=' + encodeURIComponent('http://pharmacysolutions.webflow.io/elements') + '&callback=?', function(data){
+        var $page = $(data.contents),
+            $contents = $page.filter('#humira-eyebrow');
+        
+        if ($contents.length == 0) {
+            $contents = $page.find('#humira-eyebrow');
+        }
+        
+        $("#humira-eyebrow").html($contents.html());
+    });
+    
+});
