434443b8e3c58668f00264fd7d69ee689fdd3800
Anthony <atackett@atackett.local>
2015-08-26 12:55:32 -0500

    test

--- js/global.js
+++ js/global.js
@@ -5,15 +5,15 @@ $(document).ready(function(){
         contentType: "application/json; charset=utf-8"
     });
 
-    // $.getJSON('http://whateverorigin.org/get?url=' + encodeURIComponent('http://pharmacysolutions.webflow.io/elements') + '&callback=?', function(data){
-    //     var $page = $(data.contents),
-    //         $contents = $page.filter('#humira-eyebrow');
+    $.getJSON('http://whateverorigin.org/get?url=' + encodeURIComponent('http://pharmacysolutions.webflow.io/elements') + '&callback=?', function(data){
+        var $page = $(data.contents),
+            $contents = $page.filter('#humira-eyebrow');
         
-    //     if ($contents.length == 0) {
-    //         $contents = $page.find('#humira-eyebrow');
-    //     }
+        if ($contents.length == 0) {
+            $contents = $page.find('#humira-eyebrow');
+        }
         
-    //     $("#humira-eyebrow").html($contents.html());
-    // });
+        $("#humira-eyebrow").html($contents.html());
+    });
     
 });
