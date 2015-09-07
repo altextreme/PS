db22941a33733273a97deaf5cc1937b0a6b318e7
Anthony <atackett@atackett.hsd1.mo.comcast.net>
2015-09-06 21:03:39 -0500

    test

--- js/global.js
+++ js/global.js
@@ -43,6 +43,7 @@ $(document).ready(function(){
 
                     if ($(this).hasClass(".w-dropdown-link")) {
                         $(this).closest(".w-dropdown").find(".nav-link").addClass(activeClass);
+                        console.log("test");
                     }
 
                 }
